8784758c95edd465277f3182954a9cc5e533c4f0
markchernov <mark.chernov@gmail.com>
2018-02-12 17:11:14 -0500

    added array resize

--- HW2/Test.java
+++ HW2/Test.java
@@ -12,25 +12,38 @@ public class Test {
         System.out.println("In Test main");
 
         System.out.println("Initial "  + ts);
-
+        //push1 10
         for(int i = 1; i <= 10; i++) {
             ts.push1(i);
         }
-
+        //pop1 5
+        for(int i = 0; i < 5; i++) {
+            System.out.println(ts.pop1());
+        }
+        // push2 10
         for(int i = 1; i <= 10; i++) {
             ts.push2(i);
         }
 
-        System.out.println("Before pop "  + ts);
+        // at this point array shoul look like this [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        // push1 15 forcig the resize
+        for(int i = 1; i <= 10; i++) {
+            ts.push1(i);
+        }
+
+        // at this point array should look like this [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        System.out.println("Before pop1 "  + ts);
 
         for(int i = 0; i < 10; i++) {
             System.out.println(ts.pop1());
         }
 
         for(int i = 0; i < 10; i++) {
-            ts.pop2();
+            System.out.println(ts.pop2());
         }
-
+        // at this point array should look like this [1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         System.out.println("After pop "  + ts);
     }
 }
--- HW2/TwoStacks.java
+++ HW2/TwoStacks.java
@@ -6,11 +6,11 @@ public class TwoStacks {
     private int[] arr;
 
     private boolean isEmpty1 = true;
-    private int top1 = 0;
+    private int top1;
     private int size1 = 10;
 
     private boolean isEmpty2 = true;
-    private int top2 = 10;
+    private int top2;
     private int size2 = 10;
 
     public TwoStacks() {
@@ -21,10 +21,29 @@ public class TwoStacks {
     //pushes x to first stack
     public void push1(int x) {
         System.out.println("Pushing1 " + x);
-        
-        if(this.top1 == this.size1) {
+        System.out.println("Old top1 " + this.top1);
+        if(this.top1 == this.size1 -1) {
+            //Option 1
+            //throw new Error("Fail to add: " + x  + ". Stack is full, pop some items");
+            //Option 2
+            //grow the array
+            int newSize = (this.size1 + this.size2) * 2;
+            int[] copy = new int [ newSize ];
+            System.out.println("Old array before copy " + Arrays.toString(this.arr));
+            System.out.println("Resized array size before copy is " + copy.length);
+            //args         src,  src_pos, dest, dest_pos, number_of_values
+            System.arraycopy( this.arr, 0, copy, 0, this.size1 );  // copy first half
+            System.out.println("copy after first half " + Arrays.toString(copy));
+            System.arraycopy( this.arr, this.size1, copy, this.size1 * 2, this.size2 );  // copy second half
+            System.out.println("copy after second half " + Arrays.toString(copy));
+            
 
-            throw new Error("Fail to add: " + x  + ". Stack is full, pop some items");
+            this.arr = copy;
+            this.size1 = size1 * 2;
+            this.size2 = size2 * 2;
+            this.top1++;
+            this.arr[this.top1] = x;
+            System.out.println("After resizing and copy and pushing  " + x + " " + this.toString());
         }
 
         else {            
@@ -46,21 +65,42 @@ public class TwoStacks {
     //pushes x to second stack
     public void push2(int x) {
         System.out.println("Pushing2 " + x);
-        if(this.top2 == this.size1 + this.size2) {
-            throw new Error("Fail to add: " + x  + ". Stack is full, pop some items");
+        System.out.println("Old top2 " + this.top2);
+        if(this.top2 == (this.size1 + this.size2) -1 ) {
+            //Option 1
+            //throw new Error("Fail to add: " + x  + ". Stack is full, pop some items");
+            //Option 2
+            //grow the array
+            int newSize = (this.size1 + this.size2) * 2;
+            int[] copy = new int [ newSize ];
+            System.out.println("Old array before copy " + Arrays.toString(this.arr));
+            System.out.println("Resized array size before copy is " + copy.length);
+            //args         src,  src_pos, dest, dest_pos, number_of_values
+            System.arraycopy( this.arr, 0, copy, 0, this.size1 );  // copy first half
+            System.out.println("copy after first half " + Arrays.toString(copy));
+            System.arraycopy( this.arr, this.size1, copy, this.size1 * 2, this.size2 );  // copy second half
+            System.out.println("copy after second half " + Arrays.toString(copy));
+            
+
+            this.arr = copy;
+            this.size1 = size1 * 2;
+            this.size2 = size2 * 2;
+            this.top2++;
+            this.arr[this.size1 + this.top2] = x;
+            System.out.println("After resizing and copy and pushing  " + x + " " + this.toString());
         }
 
         else {
            
             if(this.isEmpty2) {
                 System.out.println("isEmpty2 " + this.isEmpty2);
-                this.top2 = 10;       
-                this.arr[this.top2] = x;            
+                this.top2 = 0;       
+                this.arr[this.size1 + this.top2] = x;            
             }
             
             else  {
-                this.top2++;
-                this.arr[this.top2] = x;
+                this.top2++;;
+                this.arr[ this.size1 + this.top2] = x;
                 
             }
             this.isEmpty2 = false;
@@ -88,13 +128,14 @@ public class TwoStacks {
     }
     //pops an element from second stack and return the popped element
     public int pop2() {
+        System.out.println("Poping Top2: " + this.top2);
         if(this.isEmpty2) {
             throw new Error("Stack 2 is empty, push some items");
         }
         else {
-            int x = this.arr[this.top2];
-            this.arr[this.top2] = 0;           
-            if(this.top2 == 10) {
+            int x = this.arr[this.size1 + this.top2];
+            this.arr[ this.size1 + this.top2] = 0;           
+            if( this.size1 + this.top2 == this.size1) {
                 this.isEmpty2 = true;
             }
             else {
